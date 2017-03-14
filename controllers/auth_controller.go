736c0e043053dfba0b355eb380a42abc75913a14
Romain Braems <romain.braems@patientpop.com>
2017-03-14 15:39:18 +0100

    Fixed bugs with the private key and the password comparison

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -24,37 +24,37 @@ func NewAuthController(mgo *mgo.Database) *AuthController {
 
 
 func (ac AuthController) Authentication(c *gin.Context) {
-	privateKey, _ := ioutil.ReadFile("pushpal.rsa")
+	privateKeyFile, _ := ioutil.ReadFile("pushpal.rsa")
+	privateKey, _ := jwt.ParseRSAPrivateKeyFromPEM(privateKeyFile)
 
 	session := ac.mgo.Session.Copy()
 	defer session.Close()
 	users := ac.mgo.C(models.UsersCollection).With(session)
 
-	password := []byte(c.Param("password"))
-	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	userInput := models.User{}
+	c.Bind(&userInput) // TODO: HANDLE ERROR
+
+	user := models.User{}
+	err := users.Find(bson.M{"username": userInput.Username}).One(&user)
 	if err != nil {
 		c.Error(err)
 		return
 	}
 
-	user := models.User{}
-	err = users.Find(bson.M{"username": c.Param("username"), "password": string(hashedPassword)}).One(&user)
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userInput.Password))
 	if err != nil {
 		c.Error(err)
 		return
 	}
 
 	token := jwt.New(jwt.GetSigningMethod("RS256"))
-
 	claims := make(jwt.MapClaims)
 	// TODO: ADD EXPIRATION
 	//claims["exp"] = time.Now().Add(time.Hour * time.Duration(settings.Get().JWTExpirationDelta)).Unix()
 	claims["iat"] = time.Now().Unix()
 	claims["id"] = user.Id
-	claims["username"] = user.Username
 	token.Claims = claims
-
-	tokenString, _ := token.SignedString(privateKey)
+	tokenString, err := token.SignedString(privateKey)
 
 	c.JSON(http.StatusOK, gin.H{"status":"success", "data":gin.H{"token":tokenString}})
 	return
