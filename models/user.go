053209e8184123bc0f226908bdacf8c60863955b
Romain Braems <romain.braems@patientpop.com>
2017-03-14 11:32:15 +0100

    Fixed bug to find a user using the object id

--- models/user.go
+++ models/user.go
@@ -1,7 +1,9 @@
 package models
 
+import "gopkg.in/mgo.v2/bson"
+
 type User struct {
-	Id        string   `json:"id" bson:"_id,omitempty"`
+	Id        bson.ObjectId   `json:"id" bson:"_id,omitempty"`
 	Firstname string   `json:"firstname" bson:"firstname"`
 	Lastname  string   `json:"lastname" bson:"lastname"`
 	Address   *Address `json:"address" bson:"address"`
