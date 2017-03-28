5065862305dbb34e68fdb145b082ce2a4a66ca02
Maxence Henneron <mhenneron@seamgen.com>
2017-03-28 09:50:37 +0200

    Renamed a variable

--- server/index.go
+++ server/index.go
@@ -9,18 +9,18 @@ func (a API) SetupIndexes() {
 	database := a.Database
 
 	// Creates a list of indexes to ensure
-	indexes := make(map[*mgo.Collection][]mgo.Index)
+	collectionIndexes := make(map[*mgo.Collection][]mgo.Index)
 
 	// User indexes
 	users := database.C(models.UsersCollection)
-	indexes[users] = []mgo.Index{
+	collectionIndexes[users] = []mgo.Index{
 		{
 			Key:    []string{"email"},
 			Unique: true,
 		},
 	}
 
-	for collection, indexes := range indexes {
+	for collection, indexes := range collectionIndexes {
 		for _, index := range indexes {
 			err := collection.EnsureIndex(index)
 
