8ed18c23b91fe9ae339fb32af54a9003d4f50355
Maxence Henneron <mhenneron@seamgen.com>
2017-03-28 09:45:33 +0200

    Created the index configuration.
    I created a map of indexes per collections to make it cleaner.

--- server/index.go
+++ server/index.go
@@ -1,9 +1,34 @@
 package server
 
-func (a API) SetupIndexes() {
-	//database := a.Database
+import (
+	"github.com/dernise/base-api/models"
+	"gopkg.in/mgo.v2"
+)
 
-	// User
-	//users := database.C(models.UsersCollection)
-	//err := users.EnsureIndexKey("key1", "key2")
+func (a API) SetupIndexes() {
+	database := a.Database
+
+	// Creates a list of indexes to ensure
+	indexes := make(map[*mgo.Collection][]mgo.Index)
+
+	// User indexes
+	users := database.C(models.UsersCollection)
+	indexes[users] = []mgo.Index{
+		{
+			Key:    []string{"email"},
+			Unique: true,
+		},
+	}
+
+	for collection, indexes := range indexes {
+		for _, index := range indexes {
+			err := collection.EnsureIndex(index)
+
+			if err != nil {
+				panic("Could not create the indexes")
+				return
+			}
+		}
+	}
+
 }
