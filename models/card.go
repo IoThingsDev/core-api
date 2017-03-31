552864708511bc0d0f16eff3398ddd1acaa89f04
Maxence Henneron <mhenneron@seamgen.com>
2017-03-31 13:00:46 +0200

    Added the brand in the list of arguments

--- models/card.go
+++ models/card.go
@@ -1,10 +1,13 @@
 package models
 
+import "github.com/stripe/stripe-go"
+
 type Card struct {
-	Id       string `json:"id"`
-	Last4    string `json:"last_4"`
-	ExpMonth uint8  `json:"exp_month"`
-	ExpYear  uint16 `json:"exp_year"`
-	Name     string `json:"name"`
-	Default  bool   `json:"default"`
+	Id       string           `json:"id"`
+	Last4    string           `json:"last_4"`
+	ExpMonth uint8            `json:"exp_month"`
+	ExpYear  uint16           `json:"exp_year"`
+	Name     string           `json:"name"`
+	Brand    stripe.CardBrand `json:"brand"`
+	Default  bool             `json:"default"`
 }
